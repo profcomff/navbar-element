23b3c3dd9dd815ac765a096763a83b7e496e09f3
Dyakov Roman <ROMAN@DYAKOV.SPACE>
2022-10-06 15:58:21 +0300

    Fix Vue Router warn
    [Vue Router warn]: No match found for location with path "/timetable"

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import App from "@/App.vue"
 
-const routes = [{ path: "/", exact: true, redirect: "/timetable" }];
+const routes = [
+  { path: "/", exact: true, redirect: "/timetable" },
+  { path: "/:pathMatch(.*)*", name: "all", component: App }
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
