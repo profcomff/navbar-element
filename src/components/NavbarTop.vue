3c7df434e6dd9d651547a2c4a71f01814e2082e6
Dyakov Roman <diakov.ri16@physics.msu.ru>
2022-10-17 23:26:04 +0300

    z-index for NavbarTop

--- src/components/NavbarTop.vue
+++ src/components/NavbarTop.vue
@@ -1,16 +1,16 @@
 <template>
   <navbar-top-plain
-    class="noselect"
+    class="noselect topnavbar"
     v-bind:options="options"
     v-if="layoutName == 'plain'"
   />
   <navbar-top-back
-    class="noselect"
+    class="noselect topnavbar"
     v-bind:options="options"
     v-else-if="layoutName == 'back'"
   />
   <navbar-top-calendar
-    class="noselect"
+    class="noselect topnavbar"
     v-bind:options="options"
     v-else-if="layoutName == 'calendar'"
   />
@@ -42,6 +42,9 @@ export default {
 </script>
 
 <style scoped>
+.topnavbar {
+  z-index: 100;
+}
 .noselect {
   -webkit-touch-callout: none; /* iOS Safari */
   -webkit-user-select: none; /* Safari */
