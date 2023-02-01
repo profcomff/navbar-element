2345ad44accfb86f6dcbee17519fddd357a9c57e
Artem Netsvetaev <physphile@vk.com>
2023-02-02 00:24:25 +0300

    Configured eslint for navbar

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    extends: [
+        "eslint:recommended",
+        "plugin:vue/vue3-recommended"
+    ],
+    parserOptions: {
+        requireConfigFile: false
+    },
+    env: {
+        node: true
+    },
+}
